2a629252386e242821c1cb0b4edadb8be5fa248f
Pablo <pablo.duran.online@gmail.com>
2019-11-17 16:18:24 +0100

    get/post request changes

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask (__name__)
+
+@app.route("/contact", methods=["POST"])
+def contact():
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
+
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+
+    return render_template("success.html")
 
 @app.route("/")
 def index():
@@ -13,7 +25,8 @@ def index():
 
     return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
 
-@app.route("/boogle")
+
+@app.route("/boogle", methods=["GET"])
 def about_me():
     return render_template("boogle.html")
 
